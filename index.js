60c57ca9503714f0ee09c060e7dcfc587ccc85a5
AED6-DC23 <153659183+AED6-DC23@users.noreply.github.com>
2024-11-07 19:17:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+function toggleVisibility() {
+    let element = document.getElementById('Another');
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+        
+    } else {
+        element.style.display = 'none';
+       
+    }
+    
+}
+
+function toggleVisibility2() {
+    let element = document.getElementById('Prog');
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+    
+}
+
+function toggleVisibility3() {
+    let element = document.getElementById('Znan');
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+    
+}
+
+function toggleVisibility4() {
+    let element = document.getElementById('Uvl');
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+    
+}
